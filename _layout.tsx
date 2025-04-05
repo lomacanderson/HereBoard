f0eecabbb6969116899892424a62aa46baf10ad3
EthanHorizons <ethanbenstar@gmail.com>
2025-04-05 15:08:08 -0700

    Maybe fixed?

--- _layout.tsx
+++ _layout.tsx
@@ -1,4 +1,5 @@
 import { Stack } from 'expo-router';
+import React = require('react');
 
 export default function RootLayout() {
   return (
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import { Text, View, StyleSheet } from 'react-native';
 import { Link } from 'expo-router';
+import React = require('react');
 
 export default function Index() {
   return (
